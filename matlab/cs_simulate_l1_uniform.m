49170990f7c93fa25fdb008bf13124c2b486b1ac
William Fu <wfu@college.harvard.edu>
2018-12-16 09:11:08 -0500

    habibi

--- matlab/cs_simulate_l1_uniform.m
+++ matlab/cs_simulate_l1_uniform.m
@@ -1,20 +1,23 @@
-function [ succ ] = cs_simulate_l1_laplacian(N, alpha, rho)
+function [succ] = cs_simulate_l1_uniform(N, alpha, rho)
     %Run a trail of L1  minimization
     % Chuang  Sep  22 ,2015
     % N      - dimension  of the  signal
     % alpha  - M/N, where M is the  number  of  measurements
     % rho    - fraction  of non -zero  elements  of the  signal
     % If the  error  is less  then 1e-3, then  return 1 other  wise  return 0
-    M=round(N * alpha);
-    N_nonzero=round(N * rho);
-    r_perm=randperm(N, N_nonzero);
+    M = round(N * alpha);
+    N_nonzero = round(N * rho);
+    r_perm = randperm(N, N_nonzero);
     x_true=zeros(N,1);
     
     % samples from gaussian
     % x_true(r_perm )=randn(N_nonzero ,1);
     
     % samples from laplacian
-    x_true(r_perm) = laprnd(N_nonzero, 1, 0, 1);
+    % x_true(r_perm) = laprnd(N_nonzero, 1, 0, 1);
+    
+    % samples from uniform [-1, 1]
+    x_true(r_perm) = rand(N_nonzero, 1) * 2 - 1;
     
     
     A=randn(M,N) * sqrt(1/N);
